54898d2e0a7d333567f4e4f3fc09dfe04890893b
endink <sharping@outlook.com>
2025-09-08 16:06:19 +0800

    fix builder

--- exposed-dynamodb/src/main/kotlin/com/labijie/infra/orm/dynamodb/builder/DynamoDeleteBuilder.kt
+++ exposed-dynamodb/src/main/kotlin/com/labijie/infra/orm/dynamodb/builder/DynamoDeleteBuilder.kt
@@ -9,6 +9,7 @@
 package com.labijie.infra.orm.dynamodb.builder
 
 import com.labijie.infra.orm.dynamodb.*
+import software.amazon.awssdk.services.dynamodb.model.Delete
 import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest
 import software.amazon.awssdk.services.dynamodb.model.ReturnValue
 
@@ -19,24 +20,22 @@ class DynamoDeleteBuilder<PK, SK>(internal val table: DynamoTable<PK, SK>) {
         DynamoConditionBuilder(table)
     }
 
-    fun request(returnValue: ReturnValue = ReturnValue.NONE, customizer: DeleteRequestCustomizer? = null): DeleteItemRequest {
-
+    fun build(): Delete {
         val result = condition.buildCondition()
 
         val ctx = RenderContext(true)
         val expr = result.conditionExpression?.render(ctx)
 
-        return DeleteItemRequest.builder()
+        return Delete.builder()
             .tableName(this.tableName)
             .key(result.keys)
             .ifNullOrBlankInput(expr) { conditionExpression(it) }
             .ifNotNullOrEmpty(ctx.values) { expressionAttributeValues(it) }
             .ifNotNullOrEmpty(ctx.attributeNames) { expressionAttributeNames(it) }
-            .returnValues(returnValue)
-            .ifNotNull(customizer) {
-                it.invoke(this)
-                this
-            }
             .build()
+    }
+
+    fun request(returnValue: ReturnValue = ReturnValue.NONE, customizer: DeleteRequestCustomizer? = null): DeleteItemRequest {
+        return build().request(returnValue, customizer)
     }
 }
